2798496c52fff50cd2d690c007a81e01b20177bd
Batan Andrei <yabatan777@gmail.com>
2022-12-27 17:04:34 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 let images = [{
-    url: '../images/image 2.1.slider.png',
+    url: './images/image 2.1.slider.png',
 },{
-    url: '../images/image 2.2.slider.png',
+    url: './images/image 2.2.slider.png',
 },{
-    url: '../images/image 2.3.slider.png',
+    url: './images/image 2.3.slider.png',
 }];
 
 
@@ -11,17 +11,36 @@ function initSlider(){
     if(!images || !images.length) return;
 
     let sliderImages = document.querySelector(".galery");
-    let sliderArrows = document.querySelector(".second-section_arrow");
+    let sliderArrows = document.querySelector(".cub3__second-section_arrows");
 
     initImages();
-    //initArrows();
+    initArrows();
 
     function initImages(){
        images.forEach((image, index) => {
-        let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+        let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url('${images[index].url}');" data-index="${index}"></div>`;
         sliderImages.innerHTML += imageDiv;
        })
     };
+    function initArrows(){
+        sliderArrows.querySelectorAll(".second-section_arrow").forEach(arrow => {
+           arrow.addEventListener('click', function(){
+            let curNumber = +sliderImages.querySelector('.active').dataset.index;
+            let nextNumber;
+              if(arrow.classList.contains('left')) {
+                nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
+              } else {
+                nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+              }
+              moveSlider(nextNumber);
+           });
+        });
+    }
+
+    function moveSlider(num) {
+        sliderImages.querySelector('.active').classList.remove('active');
+        sliderImages.querySelector('.n' + num).classList.add('active');
+    }
 };
 
 document.addEventListener("DOMContentLoaded", function() {
