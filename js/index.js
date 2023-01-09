2dda0f7416adbb739c5da1288398a852a3fe6ad3
Batan Andrei <yabatan777@gmail.com>
2023-01-09 17:33:12 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 let images = [{
     url: './images/image 2.1.slider.png',
     text_link: 'Rostov-on-Don, Admiral',
-    text_info1: 'Rostov-on-Don LCD admiral',
-    text_info2: '81 m2',
-    text_info3: '3.5 months',
+    text_info_city: 'Rostov-on-Don LCD admiral',
+    text_info_time: '3.5 months',
+    text_info_area: '81 m2',
+    
 },{
     url: './images/image 2.2.slider.png',
     text_link: 'Sochi Thieves',
-    text_info1: 'Sochi Thieves',
-    text_info2: '105 m2',
-    text_info3: '4 months',
+    text_info_city: 'Sochi Thieves',
+    text_info_time: '4 months',
+    text_info_area: '105 m2',
+    
 },{
     url: './images/image 2.3.slider.png',
     text_link: 'Rostov-on-Don Patriotic',
-    text_info1: 'Rostov-on-Don Patriotic',
-    text_info2: '93 m2',
-    text_info3: '3 months',
+    text_info_city: 'Rostov-on-Don Patriotic',
+    text_info_time: '3 months',
+    text_info_area: '93 m2',
+    
 }];
 
 
@@ -25,14 +28,18 @@ function initSlider(){
     let sliderImages = document.querySelector(".galery");
     let sliderArrows = document.querySelector(".cub3__second-section_arrows");
     let sliderDots = document.querySelector(".second-section_dot");
-    let sliderText = document.querySelector(".navigation__menu2");
+    let sliderLink = document.querySelector(".navigation__menu2");
+    let sliderTextCity = document.querySelector(".second-section_text_info_city");
+    // let sliderTextTime = document.querySelector(".second-section_text_info_time");
+    // let sliderTextArea = document.querySelector(".second-section_text_info_area");
 
     initImages();
     initArrows();
     initDots();
     initTextLink();
-
-
+    initTextInfoCity();
+    // initTextInfoTime();
+    // initTextInfoArea();
 
 
     function initImages(){
@@ -41,9 +48,6 @@ function initSlider(){
         sliderImages.innerHTML += imageDiv;
        })
     };
-
-
-
 
     function initArrows(){
         sliderArrows.querySelectorAll(".second-section_arrow").forEach(arrow => {
@@ -60,9 +64,6 @@ function initSlider(){
         });
     }
 
-
-
-      
     function initDots(){
         images.forEach((image, index) => {
            let dot = `<div class="second-section_dot-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
@@ -71,37 +72,67 @@ function initSlider(){
         sliderDots.querySelectorAll(".second-section_dot-item").forEach(dot => {
             dot.addEventListener("click", function() {
                 moveSlider(this.dataset.index);
+
             })
     })
-
 }
-
-
 
      function initTextLink(){
 
         images.forEach((image, index) => {
             let text = `<li><a class="navigation__item2 title n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].text_link}</a></li>`;
-            sliderText.innerHTML += text;
+            sliderLink.innerHTML += text;
          });
         
-        sliderText.querySelectorAll(".navigation__item2").forEach(text => {
+         sliderLink.querySelectorAll(".navigation__item2").forEach(text => {
            //console.log(text);
     text.addEventListener("click", function() {
         moveSlider(this.dataset.index);
-      
+  
  })
 });
 }  
 
+     function initTextInfoCity() {
+         images.forEach((image, index) => {
+             let textDivCity = `<div class="cubs__paragraf-bottom subtitle n${index} ${index === 0? "active" : ""}"  data-index="${index}">${images[index].text_info_city}</div>`;
+             sliderTextCity.innerHTML += textDivCity;
+             //console.log(index);
+         });
+         moveSlider(this.dataset.index);
+}
+
+    //   function initTextInfoTime() {
+    //      images.forEach((image, index) => {
+    //          let textDivTime = `<div class="cubs__paragraf-bottom subtitle n${index} ${index === 0? "active" : ""}"  data-index="${index}">${images[index].text_info_time}</div>`;
+    //          sliderTextTime.innerHTML += textDivTime;
+    //          //console.log(index);
+    //      });
+    //    moveSlider(this.dataset.index);
+//}
+
+//        function initTextInfoArea() {
+//           images.forEach((image, index) => {
+//               let textDivArea = `<div class="cubs__paragraf-bottom subtitle n${index} ${index === 0? "active" : ""}"  data-index="${index}">${images[index].text_info_area}</div>`;
+//               sliderTextArea.innerHTML += textDivArea;
+//         //console.log(index);
+//     });
+//   moveSlider(this.dataset.index);
+// }
 
     function moveSlider(num) {
         sliderImages.querySelector('.active').classList.remove('active');
         sliderImages.querySelector('.n' + num).classList.add('active');
-        sliderText.querySelector('.active').classList.remove('active');
-        sliderText.querySelector('.n' + num).classList.add('active');
+        sliderLink.querySelector('.active').classList.remove('active');
+        sliderLink.querySelector('.n' + num).classList.add('active');
         sliderDots.querySelector('.active').classList.remove('active');
         sliderDots.querySelector('.n' + num).classList.add('active');
+        sliderTextCity.querySelector('.active').classList.remove('active');
+        sliderTextCity.querySelector('.n' + num).classList.add('active');
+        // sliderTextTime.querySelector('.active').classList.remove('active');
+        // sliderTextTime.querySelector('.n' + num).classList.add('active');
+        // sliderTextArea.querySelector('.active').classList.remove('active');
+        // sliderTextArea.querySelector('.n' + num).classList.add('active');
     }
 };
 
